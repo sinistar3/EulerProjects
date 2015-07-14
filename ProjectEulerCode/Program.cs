1a789e0c04292c018c4558032cec713ba96ac9be
Tsalic Rose <rdy4nething@gmail.com>
2015-07-13 20:47:30 -0400

    Puzzle 1 solved, answer of 233168

---
+++ ProjectEulerCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerCode
+{
+    class ProjectEuler
+    {
+        static void Main(string[] args)
+        {
+            sumsOfThreesAndFivesMultiples();
+            Console.ReadLine();
+        }
+
+        private static void sumsOfThreesAndFivesMultiples()
+        {
+            int x = 3;
+            int total = 0;
+            while (x < 1000)
+            {
+                if (x % 3 == 0 || x % 5 == 0)
+                    total += x++;
+                else x++;
+            }
+            Console.WriteLine(total); //233168
+        }
+    }
+}
